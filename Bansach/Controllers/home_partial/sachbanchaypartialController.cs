d384e9f0b844fbbf2e881eed8112cb7aa2a6328c
kieu2169 <kieuvo060921@gmail.com>
2022-01-02 23:05:24 +0700

    update

---
+++ Bansach/Controllers/home_partial/sachbanchaypartialController.cs
@@ -0,0 +1,20 @@
+﻿using Bansach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bansach.Controllers.home_partial
+{
+    public class sachbanchaypartialController : Controller
+    {
+        Bán_SáchEntities db = new Bán_SáchEntities();
+        // GET: sachbanchaypartial
+        public ActionResult Index()
+        {
+            var lstSachbanchay = db.SACHes.Take(7).ToList();
+            return PartialView(lstSachbanchay);
+        }
+    }
+}
